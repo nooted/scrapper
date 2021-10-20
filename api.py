4264ed9d443c983f3e273f25ebc2a8c1a8cb4143
Noo0teD <78922983+nooted@users.noreply.github.com>
2021-10-20 07:11:52 -0700

    Update api.py

--- api.py
+++ api.py
@@ -1,7 +1,7 @@
 import requests
 
 list = {"Geo News": "UC_vt34wimdCzdkrzVejwX9g" , "ARY NEWS": "UCMmpLL2ucRHAXbNHiCPyIyg" ,"DUNYA NEWS": "UCnMBV5Iw4WqKILKue1nP6Hg", "HUM NEWS": "UC0Um3pnZ2WGBEeoA3BX2sKw", "SAMAA NEWS": "UCJekW1Vj5fCVEGdye_mBN6Q"}
-api_key = 'AIzaSyAWGJP_Pzfl32oDLCSu27Bic5EWhUuRpnw'
+api_key = ''
 
 for the_key, the_value in list.items():
   resp = requests.get(f'https://www.googleapis.com/youtube/v3/search?part=snippet&channelId={the_value}&eventType=live&type=video&key={api_key}')
